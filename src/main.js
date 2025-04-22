0f9cecb725c38b7220b7a8ab2e124fe537439d11
hanmabaki <hongchris96@hotmail.com>
2025-04-22 13:42:22 -0700

    Remove hard code, Apply javascript to template

--- src/main.js
+++ src/main.js
@@ -1,1 +1,78 @@
+import { ICON_MAP } from "./iconMap"
+import { getWeather, getSampleWeather } from "./weather"
 import "./style.css"
+
+// getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
+//     res => {
+//         console.log(res)
+//     }
+// )
+
+
+getSampleWeather()
+    .then(renderWeather)
+    .catch(e => {
+        console.error(e)
+        alert("Error Boop")
+    })
+
+
+function renderWeather({current, daily, hourly}) {
+    renderCurrentWeather(current)
+    renderDailyWeather(daily)
+    renderHourlyWeather(hourly)
+    document.body.classList.remove("blurred")
+}
+
+function setVal(selector, value, { parent = document } = {}) {
+    parent.querySelector(`[data-${selector}]`).textContent = value
+}
+
+function getIconUrl(iconCode) {
+    return `icons/${ICON_MAP.get(iconCode)}.svg`
+}
+
+const currentIcon = document.querySelector("[data-current-icon]")
+function renderCurrentWeather(current) {
+    currentIcon.src = getIconUrl(current.iconCode)
+    setVal("current-temp", current.currentTemp)
+    setVal("current-high", current.highTemp)
+    setVal("current-low", current.lowTemp)
+    setVal("current-fl-high", current.highFeelsLike)
+    setVal("current-fl-low", current.lowFeelsLike)
+    setVal("current-wind", current.windSpeed)
+    setVal("current-precip", current.precip)
+}
+
+const DAY_FORMATTER = new Intl.DateTimeFormat(undefined, { weekday: "long" })
+const dailySection = document.querySelector("[data-day-section]")
+const dailyCardTemplate = document.getElementById("day-card-template")
+
+function renderDailyWeather(daily) {
+    dailySection.innerHTML = ""
+    daily.forEach(day => {
+        const element = dailyCardTemplate.content.cloneNode(true)
+        setVal("temp", day.maxTemp, { parent: element })
+        setVal("date", DAY_FORMATTER.format(day.timestamp), { parent: element })
+        element.querySelector("[data-icon]").src = getIconUrl(day.iconCode)
+        dailySection.append(element)
+    })
+}
+
+const HOUR_FORMATTER = new Intl.DateTimeFormat(undefined, { hour: "numeric" })
+const hourlySection = document.querySelector("[data-hour-section]")
+const hourlyCardTemplate = document.getElementById("hour-row-template")
+function renderHourlyWeather(hourly) {
+    hourlySection.innerHTML = ""
+    hourly.forEach(hour => {
+        const element = hourlyCardTemplate.content.cloneNode(true)
+        setVal("day", DAY_FORMATTER.format(hour.timestamp), { parent: element })
+        setVal("time", HOUR_FORMATTER.format(hour.timestamp), { parent: element })
+        setVal("temp", hour.temp, { parent: element })
+        setVal("fl-temp", hour.feelsLike, { parent: element })
+        setVal("wind", hour.windSpeed, { parent: element })
+        setVal("precip", hour.precip, { parent: element })
+        element.querySelector("[data-icon]").src = getIconUrl(hour.iconCode)
+        hourlySection.append(element)
+    })
+}
---
+++ src/weather.js
@@ -0,0 +1,81 @@
+import axios from "axios";
+// https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&current=temperature_2m,wind_speed_10m,weather_code,precipitation&timezone=America%2FLos_Angeles&timeformat=unixtime&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch
+
+export function getSampleWeather() {
+    return axios.get(
+        "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&current=temperature_2m,wind_speed_10m,weather_code,precipitation&timezone=America%2FLos_Angeles&timeformat=unixtime&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch"
+    ).then(({data}) => {
+        return {
+            current: parseCurrentWeather(data),
+            daily: parseDailyWeather(data),
+            hourly: parseHourlyWeather(data),
+        }
+    })
+}
+
+// Need to fix params
+export function getWeather(lat, lon, timezone) {
+    return axios.get("https://api.open-meteo.com/v1/forecast", {
+        params: {
+            latitude: lat,
+            longitude: lon,
+            timezone: timezone
+        }
+    }).then(({data}) => {
+        return {
+            current: parseCurrentWeather(data),
+            daily: parseDailyWeather(data),
+            hourly: parseHourlyWeather(data),
+        }
+    })
+};
+
+function parseCurrentWeather({ current, daily }) {
+    const {
+        temperature_2m: currentTemp,
+        wind_speed_10m: windSpeed,
+        weather_code: iconCode
+    } = current
+
+    const {
+        temperature_2m_max: [maxTemp],
+        temperature_2m_min: [minTemp],
+        apparent_temperature_max: [maxFeelsLike],
+        apparent_temperature_min: [minFeelsLike],
+        precipitation_sum: [precip]
+    } = daily
+
+    return {
+        currentTemp: Math.round(currentTemp),
+        highTemp: Math.round(maxTemp),
+        lowTemp: Math.round(minTemp),
+        highFeelsLike: Math.round(maxFeelsLike),
+        lowFeelsLike: Math.round(minFeelsLike),
+        windSpeed: Math.round(windSpeed),
+        precip: Math.round(precip * 100) / 100,
+        iconCode: iconCode
+    }
+}
+
+function parseDailyWeather({daily}) {
+    return daily.time.map((time, index) => {
+        return {
+            timestamp: time * 1000,
+            iconCode: daily.weather_code[index],
+            maxTemp: Math.round(daily.temperature_2m_max[index])
+        }
+    })
+}
+
+function parseHourlyWeather({hourly, current}) {
+    return hourly.time.map((time, index) => {
+        return {
+            timestamp: time * 1000,
+            iconCode: hourly.weather_code[index],
+            temp: Math.round(hourly.temperature_2m[index]),
+            feelsLike: Math.round(hourly.apparent_temperature[index]),
+            windSpeed: Math.round(hourly.wind_speed_10m[index]),
+            precip: Math.round(hourly.precipitation[index] * 100) / 100
+        }
+    }).filter(({timestamp}) => timestamp >= current.time * 1000)
+}
